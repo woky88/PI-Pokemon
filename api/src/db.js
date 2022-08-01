d6e5f1c82046707f798f635ffe051a71fd9197da
SergioMoyano <smoyano07@hotmail.com.ar>
2022-08-01 13:06:42 -0300

    Models Ok

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+const types = require('./models/Type');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
@@ -30,10 +31,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Pokemon, Type } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Pokemon.belongsToMany(Type, { through: 'PokemonType' })
+Type.belongsToMany(Pokemon, { through: 'PokemonType' })
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,49 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    img: {
+      type: DataTypes.STRING,
+    },
+    hp: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    heigth: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    createdPokemonDb: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  }, {
+    timestamps: false,
+    freezeTableName: true
   });
 };
