13218b4faff21c89300b9715b0b45d6af1a3901d
SergioMoyano <smoyano07@hotmail.com.ar>
2022-08-19 19:41:12 -0300

    path install

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const path = require('path');
 
 require('./db.js');
 
@@ -23,6 +24,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.use('/', routes);
 
